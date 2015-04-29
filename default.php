fba89dde3e09516fc8a6ac9fd619953ba3078741
Erwin <dams1404@student.nhl.nl>
2015-04-30 00:27:49 +0200

    Geen idee

---
+++ default.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+        <meta name="description" content="">
+        <meta name="author" content="">
+        <link rel="icon" href="../../favicon.ico">
+
+        <title>Static Top Navbar Example for Bootstrap</title>
+
+        <!-- Bootstrap core CSS -->
+        <link href="bootstrap/bootstrap.css" rel="stylesheet">
+        <!-- Custom styles -->
+        <link href="style.css" rel="stylesheet">
+    </head>
+    <body>
+
+        <?php
+            include "Shared/navbar.php";
+        ?>
+
+        <div class="container">
+            <!--main content-->
+        </div>
+
+        <!-- Bootstrap core JavaScript
+        Placed at the end of the document so the pages load faster -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+        <script src="bootstrap/bootstrap.js"></script>
+    </body>
+</html>
