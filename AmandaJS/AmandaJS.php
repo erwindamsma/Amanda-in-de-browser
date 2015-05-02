e3c55559720a0600c766f0d08c7f03cc8313caa0
Erwin <dams1404@student.nhl.nl>
2015-05-02 11:38:25 +0200

    Deleted unnecessary stuff
    - canvas
    - canvas controls
    - progress bar
    - references in js to the canvas and progress bar

--- AmandaJS/AmandaJS.php
+++ AmandaJS/AmandaJS.php
@@ -101,27 +101,27 @@
     <div class="spinner" id='spinner'></div>
     <div class="emscripten" id="status">Downloading...</div>
 
-<span id='controls'>
+<!--<span id='controls'>
   <span><input type="checkbox" id="resize">Resize canvas</span>
   <span><input type="checkbox" id="pointerLock" checked>Lock/hide mouse pointer &nbsp;&nbsp;&nbsp;</span>
-  <span><input type="button" value="Fullscreen" onclick="Module.requestFullScreen(document.getElementById('pointerLock').checked, 
+  <span><input type="button" value="Fullscreen" onclick="Module.requestFullScreen(document.getElementById('pointerLock').checked,
                                                                             document.getElementById('resize').checked)">
   </span>
-</span>
+</span>-->
 
-    <div class="emscripten">
+<!--    <div class="emscripten">
       <progress value="0" max="100" id="progress" hidden=1></progress>
-    </div>
+    </div>-->
 
-    
-    <div class="emscripten_border">
+
+    <!--<div class="emscripten_border">
       <canvas class="emscripten" id="canvas" oncontextmenu="event.preventDefault()"></canvas>
-    </div>
+    </div>-->
     <textarea id="output" rows="8"></textarea>
 
     <script type='text/javascript'>
       var statusElement = document.getElementById('status');
-      var progressElement = document.getElementById('progress');
+      //var progressElement = document.getElementById('progress');
       var spinnerElement = document.getElementById('spinner');
 
       var Module = {
@@ -152,16 +152,16 @@
             console.error(text);
           }
         },
-        canvas: (function() {
-          var canvas = document.getElementById('canvas');
+        //canvas: (function() {
+          //var canvas = document.getElementById('canvas');
 
           // As a default initial behavior, pop up an alert when webgl context is lost. To make your
           // application robust, you may want to override this behavior before shipping!
           // See http://www.khronos.org/registry/webgl/specs/latest/1.0/#5.15.2
-          canvas.addEventListener("webglcontextlost", function(e) { alert('WebGL context lost. You will need to reload the page.'); e.preventDefault(); }, false);
+          //canvas.addEventListener("webglcontextlost", function(e) { alert('WebGL context lost. You will need to reload the page.'); e.preventDefault(); }, false);
 
-          return canvas;
-        })(),
+          //return canvas;
+        //})(),
         setStatus: function(text) {
           if (!Module.setStatus.last) Module.setStatus.last = { time: Date.now(), text: '' };
           if (text === Module.setStatus.text) return;
@@ -170,14 +170,14 @@
           if (m && now - Date.now() < 30) return; // if this is a progress update, skip it if too soon
           if (m) {
             text = m[1];
-            progressElement.value = parseInt(m[2])*100;
-            progressElement.max = parseInt(m[4])*100;
-            progressElement.hidden = false;
+            //progressElement.value = parseInt(m[2])*100;
+            //progressElement.max = parseInt(m[4])*100;
+            //progressElement.hidden = false;
             spinnerElement.hidden = false;
           } else {
-            progressElement.value = null;
-            progressElement.max = null;
-            progressElement.hidden = true;
+            //progressElement.value = null;
+            //progressElement.max = null;
+            //progressElement.hidden = true;
             if (!text) spinnerElement.style.display = 'none';
           }
           statusElement.innerHTML = text;
