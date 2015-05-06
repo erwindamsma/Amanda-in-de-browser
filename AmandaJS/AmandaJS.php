42de5e864060512f787d2b579f0657e931da5f0f
Erwin <dams1404@student.nhl.nl>
2015-05-06 14:52:02 +0200

    Deleted commented code

--- AmandaJS/AmandaJS.php
+++ AmandaJS/AmandaJS.php
@@ -3,23 +3,6 @@
 <div class="spinner" id='spinner'></div>
 <div class="emscripten" id="status">Downloading...</div>
 
-<!--<span id='controls'>
-  <span><input type="checkbox" id="resize">Resize canvas</span>
-  <span><input type="checkbox" id="pointerLock" checked>Lock/hide mouse pointer &nbsp;&nbsp;&nbsp;</span>
-  <span><input type="button" value="Fullscreen" onclick="Module.requestFullScreen(document.getElementById('pointerLock').checked,
-                                                                            document.getElementById('resize').checked)">
-  </span>
-</span>-->
-
-<!--    <div class="emscripten">
-      <progress value="0" max="100" id="progress" hidden=1></progress>
-    </div>-->
-
-
-    <!--<div class="emscripten_border">
-      <canvas class="emscripten" id="canvas" oncontextmenu="event.preventDefault()"></canvas>
-    </div>-->
-
 <div class="row">
     <div class="col-md-12">
         <textarea id="functions" class="form-control" rows="12"></textarea>
@@ -56,15 +39,8 @@
     </div>
 </div>
 
-
-
-
-
-
-
 <script type='text/javascript'>
   var statusElement = document.getElementById('status');
-  //var progressElement = document.getElementById('progress');
   var spinnerElement = document.getElementById('spinner');
 
   var Module = {
@@ -75,11 +51,6 @@
       if (element) element.value = ''; // clear browser cache
       return function(text) {
         if (arguments.length > 1) text = Array.prototype.slice.call(arguments).join(' ');
-        // These replacements are necessary if you render to raw HTML
-        //text = text.replace(/&/g, "&amp;");
-        //text = text.replace(/</g, "&lt;");
-        //text = text.replace(/>/g, "&gt;");
-        //text = text.replace('\n', '<br>', 'g');
         console.log(text);
         if (element) {
           element.value += text + "\n";
@@ -95,16 +66,6 @@
         console.error(text);
       }
     },
-    //canvas: (function() {
-      //var canvas = document.getElementById('canvas');
-
-      // As a default initial behavior, pop up an alert when webgl context is lost. To make your
-      // application robust, you may want to override this behavior before shipping!
-      // See http://www.khronos.org/registry/webgl/specs/latest/1.0/#5.15.2
-      //canvas.addEventListener("webglcontextlost", function(e) { alert('WebGL context lost. You will need to reload the page.'); e.preventDefault(); }, false);
-
-      //return canvas;
-    //})(),
     setStatus: function(text) {
       if (!Module.setStatus.last) Module.setStatus.last = { time: Date.now(), text: '' };
       if (text === Module.setStatus.text) return;
@@ -113,14 +74,8 @@
       if (m && now - Date.now() < 30) return; // if this is a progress update, skip it if too soon
       if (m) {
         text = m[1];
-        //progressElement.value = parseInt(m[2])*100;
-        //progressElement.max = parseInt(m[4])*100;
-        //progressElement.hidden = false;
         spinnerElement.hidden = false;
       } else {
-        //progressElement.value = null;
-        //progressElement.max = null;
-        //progressElement.hidden = true;
         if (!text) spinnerElement.style.display = 'none';
       }
       statusElement.innerHTML = text;
