fa95db7166a92d18f8a2e8c3ed720ab85f31e4fb
Jens Vossnack <jensvossnack@gmail.com>
2015-05-13 13:36:04 +0200

    Revert "Added save file support"
    This reverts commit 87eea3c23057e431761aa403681be263a2e7d834.

--- default.php
+++ default.php
@@ -20,8 +20,6 @@
         Placed at the end of the document so the pages load faster -->
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
         <script src="bootstrap/bootstrap.js"></script>
-
-        <script src="https://www.dropbox.com/static/api/dropbox-datastores-1.2-latest.js" type="text/javascript"></script>
     </head>
     <body>
 
