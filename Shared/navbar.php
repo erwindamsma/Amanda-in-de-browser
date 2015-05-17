3cccc6d1003b0ad0d288472ae3a98f6128036015
Erwin <dams1404@student.nhl.nl>
2015-05-18 00:55:48 +0200

    User that log are now stored in the database

--- Shared/navbar.php
+++ Shared/navbar.php
@@ -10,6 +10,15 @@
     require_once( 'SDKs/facebook-php-sdk-v4-4.0-dev/src/Facebook/FacebookRequestException.php' );
     require_once( 'SDKs/facebook-php-sdk-v4-4.0-dev/src/Facebook/FacebookAuthorizationException.php' );
     require_once( 'SDKs/facebook-php-sdk-v4-4.0-dev/src/Facebook/GraphObject.php' );
+
+    require_once( 'SDKs/facebook-php-sdk-v4-4.0-dev/src/Facebook/Entities/AccessToken.php');
+
+    require_once('SDKs/facebook-php-sdk-v4-4.0-dev/src/Facebook/HttpClients/FacebookHttpable.php');
+    require_once('SDKs/facebook-php-sdk-v4-4.0-dev/src/Facebook/HttpClients/FacebookCurl.php');
+    require_once('SDKs/facebook-php-sdk-v4-4.0-dev/src/Facebook/HttpClients/FacebookCurlHttpClient.php');
+    require_once('SDKs/facebook-php-sdk-v4-4.0-dev/src/Facebook/FacebookOtherException.php');
+    require_once('SDKs/facebook-php-sdk-v4-4.0-dev/src/Facebook/GraphSessionInfo.php');
+
     use Facebook\FacebookSession;
     use Facebook\FacebookRedirectLoginHelper;
     use Facebook\FacebookRequest;
@@ -19,15 +28,6 @@
     use Facebook\FacebookAuthorizationException;
     use Facebook\GraphObject;
 
-    require_once( 'SDKs/facebook-php-sdk-v4-4.0-dev/src/Facebook/Entities/AccessToken.php');
-
-    require_once('SDKs/facebook-php-sdk-v4-4.0-dev/src/Facebook/HttpClients/FacebookHttpable.php');
-    require_once('SDKs/facebook-php-sdk-v4-4.0-dev/src/Facebook/HttpClients/FacebookCurl.php');
-    require_once('SDKs/facebook-php-sdk-v4-4.0-dev/src/Facebook/HttpClients/FacebookCurlHttpClient.php');
-    require_once('SDKs/facebook-php-sdk-v4-4.0-dev/src/Facebook/FacebookOtherException.php');
-    require_once('SDKs/facebook-php-sdk-v4-4.0-dev/src/Facebook/GraphSessionInfo.php');
-    require_once('SDKs/facebook-php-sdk-v4-4.0-dev/src/Facebook/GraphUser.php');
-
     FacebookSession::setDefaultApplication('1453083768317435', 'e5aea522d0911aacdac480b23d1755cd');
     $helper = new FacebookRedirectLoginHelper("http://www.edamsma.nl/");
     //$loginUrl = $helper->getLoginUrl();
@@ -36,10 +36,25 @@
         $session = $helper->getSessionFromRedirect();
     } catch (FacebookRequestException $ex) {
         // When Facebook returns an error
-        echo $ex->getMessage();
     } catch (Exception $ex) {
         // When validation fails or other local issues
-        echo $ex->getMessage();
+    }
+    if (isset($session)){
+        // graph api request for user data
+        $response = (new FacebookRequest( $session, 'GET', '/me' ))->execute();
+        // get response
+        $graphObject = $response->getGraphObject();
+
+        $db = new mysqli("10.184.18.211", "u151188_auth", "BX}5Z+2x7y");
+        $db->select_db("db151188_main");
+
+        $sql = "SELECT userId FROM users WHERE apiId = " . $graphObject->getProperty('id');
+        $result = $db->query($sql);
+        if ($result->num_rows === 0) {
+            $db->query("INSERT INTO users (apiId) VALUES (" . $graphObject->getProperty('id') . ")");
+        }
+
+        echo($db->error);
     }
 ?>
 
@@ -64,14 +79,7 @@
                 <li>
                     <?php
                         if ( isset($session) ) {
-                            // graph api request for user data
-                            $request = new FacebookRequest( $session, 'GET', '/me' );
-                            $response = $request->execute();
-                            // get response
-                            $graphObject = $response->getGraphObject();
-
-                            echo  print_r( $graphObject, 1 );
-
+                            echo print_r($graphObject, 1);
                         } else {
                             echo '<a href="' . $helper->getLoginUrl( array( 'email', 'user_friends' ) ) . '">Login</a>';
                         }
