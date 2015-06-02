70dfeaa2da85c02c658b9c1e5352059e752ae71b
SanderJaasma <jaas1400@student.nhl.nl>
2015-06-02 13:29:42 +0200

    hoi2

--- index.php
+++ index.php
@@ -122,7 +122,7 @@
                             </div>
                         </div>
                         <div class="modal-footer">
-                            <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                            <button type="button" class="btn btn-default" id="help-modal-close" data-dismiss="modal">Close</button>
                         </div>
                     </div>
                 </div>
