ded4483d19df1ae40796cf6d613c38bed1a96ff4
Erwin <dams1404@student.nhl.nl>
2015-06-02 19:39:58 +0200

    Removed a test file

--- test.php
+++
@@ -1,5 +0,0 @@
-<div style="display: block; float: left; border: 1px dashed red;">
-    <div style="display: inline-block; float: left; border: 1px solid black;">nested</div>
-    <div style="display: inline-block; float: left; border: 1px solid black;">nested</div>
-    <div style="display: inline-block; float: left; border: 1px solid black;">nested</div>
-</div>
