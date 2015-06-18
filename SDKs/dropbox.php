f9cd0d66bfe4dcbf6feac15c9c628b0ca9a119a2
Erwin <dams1404@student.nhl.nl>
2015-06-18 11:31:13 +0200

    Cleaned up

--- SDKs/dropbox.php
+++ SDKs/dropbox.php
@@ -2,18 +2,20 @@
     require_once "dropbox-sdk/lib/Dropbox/autoload.php";
     use \Dropbox as dbx;
 
+	//Start session if it isn't set
     if (!isset($_SESSION)){
         session_start();
     }
 
     function getWebAuth(){
-        $appInfo = dbx\AppInfo::loadFromJsonFile("SDKs/config.json");
-        $clientIdentifier = "AmandaJS/1.0";
-        $redirectUri = "https://edamsma.nl/amanda";
-        $csrfTokenStore = new dbx\ArrayEntryStore($_SESSION, 'dropbox-auth-csrf-token');
-        return new dbx\WebAuth($appInfo, $clientIdentifier, $redirectUri, $csrfTokenStore);
+        $appInfo = dbx\AppInfo::loadFromJsonFile("SDKs/config.json"); //Load key and secret
+        $clientIdentifier = "AmandaJS/1.0"; //Name for the client
+        $redirectUri = "https://edamsma.nl/amanda"; //Uri to redirect to after the dropbox authentication has been completed
+        $csrfTokenStore = new dbx\ArrayEntryStore($_SESSION, 'dropbox-auth-csrf-token'); //ArrayEntryStorage for the Dropbox token
+        return new dbx\WebAuth($appInfo, $clientIdentifier, $redirectUri, $csrfTokenStore); //Return the web authenticator created with the above variables
     }
 
+	//Logout if if $_REQUEST['logout'] is set and $_SESSION is set
     if (isset($_REQUEST['logout']) && isset($_SESSION)) {
         session_destroy();
         session_start();
@@ -21,18 +23,21 @@
         header('Location: ' . filter_var($redirect, FILTER_SANITIZE_URL));
     }
 
+	//When the user returns from the Dropbox login page: get the users information and fill the session
     if (isset($_GET['state']) && isset($_GET['code'])){
         try {
             list($accessToken, $userId) = getWebAuth()->finish($_GET);
 
-            $dbxClient = new dbx\Client($accessToken, "AmandaJS/1.0");
-            $accountInfo = $dbxClient->getAccountInfo();
+            $dbxClient = new dbx\Client($accessToken, "AmandaJS/1.0"); //Create the Dropbox Client
+            $accountInfo = $dbxClient->getAccountInfo(); //Get the users information
 
+			//Save some objects in the session for later use
             $_SESSION['loggedIn'] = true;
             $_SESSION['accessToken'] = $accessToken;
             $_SESSION['dbxClient'] = $dbxClient;
             $_SESSION['accountInfo'] = $accountInfo;
 
+			//Clear the url from GET requests
             $redirect = 'http://' . $_SERVER['HTTP_HOST'] . $_SERVER['PHP_SELF'];
             header('Location: ' . filter_var($redirect, FILTER_SANITIZE_URL));
         }
@@ -58,7 +63,7 @@
             error_log("/dropbox-auth-finish: error communicating with Dropbox API: " . $ex->getMessage());
         }
     } else {
-        $authorizeUrl = getWebAuth()->start();
+        $authorizeUrl = getWebAuth()->start(); //Create the URL for the login button
     }
 
 
--- dropboxupload.php
+++ dropboxupload.php
@@ -1,18 +1,14 @@
 <?php
-    /*REMOVE THIS PART BEFORE GOING LIVE AS IT ENABLES PHP ERRORS
-    ################################*/
-    error_reporting(-1);
-    ini_set('display_errors', 'On');
-    /*##############################*/
+	//Get the Dropbox SDK
+	use \Dropbox as dbx;
+	
+    //Include the Dropbox authentication file (this has to be included in every page where Dropbox is used or where the user can login or logout)
+	include 'SDKs/dropbox.php';
 
-    use \Dropbox as dbx;
-    include 'SDKs/dropbox.php';
-
-    $fileToSave = fopen($_SESSION['accountInfo']['uid'] . $_POST['fileName'], 'w');
-    fwrite($fileToSave, $_POST['editorValue']);
-    $fileToSave = fopen($_SESSION['accountInfo']['uid'] . $_POST['fileName'], 'r');
-    $uploadResult = $_SESSION['dbxClient']->uploadFile("/" . $_POST['fileName'] . ".ama", dbx\WriteMode::add(), $fileToSave);
-    fclose($fileToSave);
-    unlink($_SESSION['accountInfo']['uid'] . $_POST['fileName']);
-
+    $fileToSave = fopen($_SESSION['accountInfo']['uid'] . $_POST['fileName'], 'w'); //Create file to store the document in, in write mode
+    fwrite($fileToSave, $_POST['editorValue']); //Write to the file which has just been created
+    $fileToSave = fopen($_SESSION['accountInfo']['uid'] . $_POST['fileName'], 'r'); //Open the file which the document is stored in, in read mode
+    $uploadResult = $_SESSION['dbxClient']->uploadFile("/" . $_POST['fileName'] . ".ama", dbx\WriteMode::add(), $fileToSave); //Upload to the users Dropbox
+    fclose($fileToSave); //Close the file
+    unlink($_SESSION['accountInfo']['uid'] . $_POST['fileName']); //Delete the file from the server
 ?>
